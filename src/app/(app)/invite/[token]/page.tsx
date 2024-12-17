9fd880b097e61451e60a051423ffa3d604a360d2
Vishal Sonthalia <vishals123@gmail.com>
2024-12-17 16:03:08 +0530

    Invite token working with params

--- src/app/(app)/invite/[token]/page.tsx
+++ src/app/(app)/invite/[token]/page.tsx
@@ -1,29 +1,37 @@
-"use client";
+'use client'
 
 
-import React, { ReactElement, useEffect } from 'react'
-import SignupForm from './components/SignupForm';
-import { useParams , useRouter} from 'next/navigation';
+import React, { ReactElement, useEffect, useState } from 'react'
+import SignupForm from './components/SignupForm'
+import { useParams, useRouter } from 'next/navigation'
 import { validateInviteToken } from '@/app/(app)/invite/[token]/actions/validateInviteToken'
 
 
-export default async function Page(): Promise<ReactElement> {
-  console.log('token')
-  const params = useParams();
-  console.log(params.token)
+export default function Page(): ReactElement {
+  const [error, setError] = useState(false)
+  const params = useParams()
   const router = useRouter()
 
-  const result = await validateInviteToken({token: params.token})
-  console.log(result)
 
-  // useEffect(() => {
-  //   if(params.token && result.error){
-  //     router.push('/')
-  //   }
-  // }, [])
+  // const result = await validateInviteToken({token: params.token})
+  // console.log(result)
+
+  useEffect(() => {
+    if (params.token) {
+      console.log(params.token)
+     validateInviteToken({ token: params.token }).then(r => {
+        if (r.error) {
+          router.push('/')
+        }
+      })
+    }
+
+  }, [router])
+
+
   return (
-        <div className='h-[calc(100vh-3rem)]'>
-            invite page
-        </div>
-    );
+    <div className='h-[calc(100vh-3rem)]'>
+      invite page
+    </div>
+  )
 }
